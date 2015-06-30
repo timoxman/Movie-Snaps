1c344f9b162d0ac46db6bbc3c86ca01c541feac7
Tim Oxman <tim.oxman@gmail.com>
2015-06-30 18:00:34 +0100

    last bit of scaffolding complete for JS in rails

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,7 +15,7 @@ class LocationsController < ApplicationController
 
   # define which params we are going to allow us to pass to controller, without this security flaw.
   def location_params
-    params.require(:location).permit(:name, :description, :film, :latitude, :longitude, :address)
+    params.require(:location).permit(:name, :description, :latitude, :longitude, :address)
   end
 
   def show
