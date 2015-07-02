d745b1a6e1c66e3a275f9500aa18e83746c34032
andygout <andygout@hotmail.co.uk>
2015-07-02 18:50:52 +0100

    Adds: User profile display lists visit info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @visits = Visit.where("user_id = ?", @user.id)
   end
 
 end
+
+# Scene.where("movie_id = ? AND location_id = ?",movie.id,location.id)
