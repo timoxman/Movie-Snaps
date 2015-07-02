769aa83ea2c06334726fd4bdd60b86c5554378ac
Stefan Lissia <stefan.emailacct@gmail.com>
2015-07-02 15:12:40 +0100

    second test - checks for user id on page

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -2,16 +2,27 @@ require 'rails_helper'
 
 feature 'A user wants to view another user\'s profile page' do
 
-  before do
+  before(:all) do
     set_omniauth
     visit '/'
+    click_link 'Sign in with Facebook'
+    visit '/users/1'
   end
 
   context 'viewing page' do
     scenario 'displays user name' do
+      expect(page).to have_content 'User page of John Doe'
+
+    end
+
+    scenario 'displays user image' do
+      set_omniauth
+      visit '/'
       click_link 'Sign in with Facebook'
       visit '/users/1'
-      expect(page).to have_content 'User page of John Doe'
+      expect(page).to have_css('img#userProfile')
     end
+
+
   end
 end
