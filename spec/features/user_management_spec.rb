7d21a9d4c8254981a19b892e42c175c6ab2538c3
Daryl Watson <darylwtsn@gmail.com>
2015-07-03 11:41:14 +0100

    Fixed some tests

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -15,7 +15,8 @@ feature 'User is on the home page' do
     end
 
     scenario 'has signed in and wants to sign back out' do
-      expect(page).not_to have_content 'Welcome'
+      click_link 'Sign in with Facebook'
+      click_link 'Sign out'
       expect(page).not_to have_content 'Sign out'
       expect(page).to have_content 'Sign in with Facebook'
     end
