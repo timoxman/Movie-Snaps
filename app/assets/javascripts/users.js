cb29a1c6bd2d93acb388572c1151a24db141bc9d
andygout <andygout@hotmail.co.uk>
2015-07-03 19:11:25 +0100

    Adds: Map to user profile page visits list

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
+
   $('#visits').children('ul').children('li').each(function(index) {
     $(this).find('.movieTitleYear').hide();
     $(this).find('.longitude').hide();
     $(this).find('.latitude').hide();
+
     var movieTitleYear = $(this).find('.movieTitleYear').text();
-    var longitude = $(this).find('.longitude').text();
-    var latitude = $(this).find('.latitude').text();
     var movieTitle = movieExtractTitle(movieTitleYear);
     var movieYear = movieExtractYear(movieTitleYear);
     getPoster(movieTitle, movieYear, index);
+
+    var longitude = $(this).find('.longitude').text();
+    var latitude = $(this).find('.latitude').text();
+    loadMap(latitude, longitude, index);
   });
+
 });
 
 function movieExtractTitle(movieTitleYear){
@@ -31,3 +36,16 @@ function getPoster(movieTitle, movieYear, index) {
     $(listItem).append(poster);
   })
 }
+
+function loadMap(latitude, longitude, index) {
+  var myOptions2 = {
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var latlng = new google.maps.LatLng(latitude, longitude);
+  var id = 'mapDestination-' + (index + 1)
+  map = new google.maps.Map(document.getElementById(id), myOptions2);
+  map.setCenter(latlng);
+  map.setOptions({ draggableCursor: 'crosshair' });
+};
