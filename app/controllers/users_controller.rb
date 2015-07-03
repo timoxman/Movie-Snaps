58982869e4f416947780aca014d484780ac444eb
andygout <andygout@hotmail.co.uk>
2015-07-03 10:34:15 +0100

    Resolves: javascripts called to respective controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
     @visits = Visit.where("user_id = ?", @user.id)
   end
 
-end
+  def movies_api
+    movies = []
+    User.find(params[:id]).visited_scenes.each do |visited_scene|
+      movies << visited_scene.movie
+    end
+    render json: movies.to_json
+  end
 
-# Scene.where("movie_id = ? AND location_id = ?",movie.id,location.id)
+  def locations_api
+    locations = []
+    User.find(params[:id]).visited_scenes.each do |visited_scene|
+      locations << visited_scene.location
+    end
+    render json: locations.to_json
+  end
+
+end
