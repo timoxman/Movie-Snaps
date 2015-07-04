bbefe79f1a21bd1341b656c10d22b9815f03f824
Daryl Watson <darylwtsn@gmail.com>
2015-07-04 20:35:25 +0100

    Can like a photo once per user, provided page is refreshed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    # byebug
     @photos = Photo.all
   end
+
 end
--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -8,11 +8,18 @@ feature 'A user on the homepage' do
       create_visit
     end
 
-    scenario 'can like another users photo', js: true do
+    scenario 'can like a photo', js: true do
       visit '/'
       click_link 'Like'
       expect(page).to have_content '1 like'
       expect(Like.count).to eq 1
+    end
+
+    scenario 'can only like a photo once', js: true do
+      visit '/'
+      click_link 'Like'
+      expect(page).not_to have_link 'Like'
+      expect(page).to have_content 'You liked this'
     end
 
   end
@@ -25,7 +32,7 @@ feature 'A user on the homepage' do
       click_link 'Sign out'
     end
 
-    scenario 'cannot like another users photo', js: true do
+    scenario 'cannot like a photo', js: true do
       expect(page).not_to have_link 'Like'
     end
 
