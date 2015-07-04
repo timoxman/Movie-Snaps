091779ac0033b2126675a1574fea529939ccec1e
Daryl Watson <darylwtsn@gmail.com>
2015-07-04 17:02:18 +0100

    Can only comment when signed in

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 feature 'A user on the homepage' do
 
-  before(:each) do
-    create_visit
-  end
-
   context 'while logged in' do
+
+    before(:each) do
+      create_visit
+    end
+
     scenario 'can comment on other users photo', js: true do
       click_link 'Comment'
       expect(page).to have_content 'Leave a comment'
@@ -14,7 +15,29 @@ feature 'A user on the homepage' do
       click_button 'Comment'
       expect(Comment.count).to eq 1
       expect(current_path).to eq root_path
-      # expect(page).to have_content 'Nice photo!'
+      expect(page).to have_content 'Nice photo! John Doe'
     end
+
+  end
+
+  context 'while logged out' do
+
+    before(:each) do
+      create_visit
+      p = Photo.first
+      u = User.first
+      Comment.create(remark: 'Nice photo!', photo_id: p.id, user_id: u.id)
+      visit '/'
+      click_link 'Sign out'
+    end
+
+    scenario 'is not allowed to comment' do
+      expect(page).not_to have_content 'Comment'
+    end
+
+    scenario 'can see comments already created' do
+      expect(page).to have_content 'Nice photo! John Doe'
+    end
+
   end
 end
