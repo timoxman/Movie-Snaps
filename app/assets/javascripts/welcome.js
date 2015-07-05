ba87bd67911ee3961dce40b919332097ca75d2fa
Daryl Watson <darylwtsn@gmail.com>
2015-07-05 15:29:37 +0100

    Fixed the liking bug

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   $('.likes-link').on('click', function(event){
     event.preventDefault();
     $(this).hide();
-    var likeCount = $(this).siblings('.likes_count');
+    var likeCount = $(this).next('.likes_count');
 
     $.post(this.href, function(response){
       likeCount.text(response.new_like_count);
