16c7e0964e6bf3a6e3206eeeed6bb7a30a5c0d59
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-07-05 15:38:41 +0100

    ADDS: movie and location pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,9 @@ class MoviesController < ApplicationController
     scene = Scene.create(movie_id: film.id, location_id: location)
     redirect_to new_location_scene_visit_path(location, scene.id)
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
 end
