1e88e8ea842bfe2bfe226a3d96c868563b622982
Ashleigh Maund <ashleighmaund@gmail.com>
2015-07-06 15:58:40 +0100

    committing all changes

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -9,7 +9,7 @@ describe Photo do
     set_omniauth
     Location.create(id:1,latitude:0,longitude:0,description:"This is a location")
     Movie.create(id:1,name:"Dude, Where's My Test?")
-    Scene.create(id:1,,image_file_name:"some.jpg")
+    Scene.create(id:1,image_file_name:"some.jpg")
     Visit.create(id:1,date_visited:"2015-07-03")
     expect(Photo.create(image_file_name:"some.jpg", visit_id:1)).to be_valid
   end
