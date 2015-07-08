f9e9c8962ae816eb349f07bc91b86fb93fd95c65
Ashleigh Maund <ashleighmaund@gmail.com>
2015-07-08 09:39:35 +0100

    added failing test

--- spec/features/scene_feature_spec.rb
+++ spec/features/scene_feature_spec.rb
@@ -17,8 +17,4 @@ feature 'User clicks on a previously added location' do
     expect(page).to have_content 'Tell us something about your visit'
   end
 
-  scenario 'and can add a new scene' do
-    visit 'scenes/new'
-  end
-
 end
